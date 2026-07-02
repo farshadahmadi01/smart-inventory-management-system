c6d5017b99e92a8101ded1e7814aae23665eda47
farshadahmadi01 <farshadahmadi6991@gmail.com>
2026-07-02 12:39:28 +0330

    Initialize project structure

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def main():
+    print("=" * 50)
+    print("Smart Inventory & Sales Management System")
+    print("=" * 50)
+    print("System started successfully.")
+
+if __name__=="__main__":
+    main()
